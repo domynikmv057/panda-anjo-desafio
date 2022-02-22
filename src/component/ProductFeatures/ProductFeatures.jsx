06d498ef257e4fa569df4c3ab2d4bb9268e6213f
domynikmv057 <domynikmv.057@gmail.com>
2022-02-22 16:53:50 -0300

    feat: add product features component

---
+++ src/component/ProductFeatures/ProductFeatures.jsx
@@ -0,0 +1,34 @@
+import "./ProductFeatures.css";
+import LowCarb from "../../assets/svgIcons/LowCarb.svg";
+import NoDairy from "../../assets/svgIcons/NoDairy.svg";
+import NoGluten from "../../assets/svgIcons/NoGluten.svg";
+import Organic from "../../assets/svgIcons/Organic.svg";
+import foodBack from "../../assets/product/FoodBackground 1366x421.png";
+export const ProductFeatures = () => {
+  return (
+    <section>
+      <div className="main-container-features">
+        <div className="svg-container">
+          <img src={NoGluten} alt="sem glútem" className="big-svg" />
+          <span>Sem Glúten</span>
+        </div>
+
+        <div className="svg-container">
+          <img src={Organic} alt="organico" className="big-svg" />
+          <span>Orgânico</span>
+        </div>
+
+        <div className="svg-container">
+          <img src={LowCarb} alt="lowCarb" className="normal-svg" />
+          <span>LowCarb</span>
+        </div>
+
+        <div className="svg-container">
+          <img src={NoDairy} alt="sem lacteos" className="normal-svg" />
+          <span>Sem Lácteos</span>
+        </div>
+      </div>
+      <div className="food-background"></div>
+    </section>
+  );
+};
